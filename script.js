0a8a17b56cb2b91cc154e938803dc7d73e9d8e62
Carlosior <carlos-ignacio11@hotmail.com>
2025-06-12 14:30:35 -0500

    English page for Teacher.

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Thank you for contacting me! I will get back to you soon.');
+  this.reset();
+});
